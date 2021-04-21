34c6c0c3388cf831b9c30025e540401fd2276adf
pinky0703 <pinky0703@naver.com>
2021-04-21 20:26:53 +0900

    04-21 코드리팩토링

---
+++ src/js/DBHelper.js
@@ -0,0 +1,56 @@
+class DBHelper{
+    #dataList = [];
+    #KEY = '';
+    constructor(KEY){
+        this.#KEY = KEY;
+        let strTodo = localStorage.getItem(this.#KEY); 
+        this.#dataList = (strTodo === null) ? []:  JSON.parse(strTodo);
+    }
+    
+    /**
+     * #dataList 리턴 
+     */
+    get(){
+        return this.#dataList;
+    }
+
+    /**
+     * localStorage 에 KEY 값으로 #dataList 를 문자열로 저장
+     */
+    save(){
+        localStorage.setItem(this.#KEY, JSON.stringify(this.#dataList));
+    }
+
+    /**
+     * #dataList 에 item 추가 후 save()
+     * @param {json} item 
+     */
+    addItem(item){
+        this.#dataList.push(item);
+        this.save();
+    }
+
+    /**
+     * itemId 에 해당하는 item 의 완료체크 업데이트 
+     * @param {string} itemId 
+     * @param {boolean} isChecked 
+     */
+    updateIsDone(itemId, isChecked){
+        
+        this.#dataList.filter(f=>f.id==itemId).forEach(it => it.isDone = isChecked);
+        this.save();
+        // 회색으로 바꿔주는거 
+        // this.#dataList.filter(f=>f.id==itemId).forEach(it => it.isDone = isChecked);
+    }
+
+    /**
+     * itemId 에 해당하는 item 삭제 
+     * @param {string} itemId 
+     */
+    removeItem(itemId){
+        this.#dataList = this.#dataList.filter(f=>f.id !== itemId); 
+        $(this).parent().remove(); 
+        this.save();
+    }
+}
+
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,26 +1,13 @@
-// json 형식을 전역변수로 만든다
-var todoList = [];
-// todo list 에 있는 key 값을 여기다 넣어주고, value값을 리스트에 뿌려줄거임 
-const DB_KEY = 'todoList';
+var db = new DBHelper("todoList");
 /**
  * loadTodoList
  * 페이지 로드 다된다음에 localStorage의 데이터를 리스트로 뿌려줌 
  * @param {String} todoText 
  */
-  function  loadTodoList(){ // 
-    let strTodo = localStorage.getItem(DB_KEY); 
-    if(strTodo === null){
-        strTodo = [];
-    }
-    else
-        todoList = JSON.parse(strTodo);
-         // parse 했을때 json 배열일수도 있고 그냥 배열일수도 있대 
+  function  loadTodoList(){ 
+    db.get();
+  }
 
-  }
-  function saveTodoList(){
-      localStorage.setItem(DB_KEY, JSON.stringify(todoList));
-      localStorage.getItem(DB_KEY, JSON.stringify(todoList));
-  }
 
   
   function addListItem(item){
@@ -42,7 +29,7 @@ const DB_KEY = 'todoList';
 
 function listUp(){
 
-  todoList.forEach(item=>{
+  db.get().forEach(item=>{
       console.log(item.text);
       addListItem(item);
   });
@@ -63,10 +50,8 @@ function addTodo(todoText){
         'isDone' : false //  완료 됐는지 안됐는지 ? 
     }
     // list에 추가 
-    addListItem(item);
-    
-    todoList.push(item); // js 에다가 푸시하고ㅡ, 로컬스토리지에 save 해야돼 
-    saveTodoList(); // 정합성에 어긋날수도 있으니까 이거 두개 같이 세트로 들고 다녀야돼 
+    addListItem(item); // UI 표시 
+    db.addItem(item); 
 }
 var isDigit = function(str){
     try{
@@ -77,12 +62,6 @@ var isDigit = function(str){
     }
    
 }
-function deleteItem(item){
-
-    arr = arr.filter(item => item !== value);
-    saveTodoList();
-}
-
 $(document).ready(function(){
     $('#todoInput').keydown(function(e){
         if(e.keyCode === 13){
@@ -94,47 +73,23 @@ $(document).ready(function(){
     $(this).on('change','.todoCheck',function(e){
         // data-id 갖고오는 법 ? 
         let itemId = $(this).parent().data('id'); // 속성이 data-id 의 값을 가져옴 //  콘솔로 문법 확인가능함 
-        
         let isChecked = $(this).prop('checked');
-        
-        todoList.filter(f=>f.id==itemId).forEach(it => it.isDone = isChecked);
-
-        // 체크된 항목 그레이로 색 변경 
-        if(isChecked)
-            $(this).parent().css('color','gray');
-        else
-            $(this).parent().css('color','black');
-        // localStorage에 동기화(저장)
-        localStorage.setItem()
-        saveTodoList(); 
+        db.updateIsDone(itemId, isChecked);
 
         // 색도 새로 고침해도 저장되게! 
     });
 
     $(this).on('click','.todo-del-btn',function(e){
         let itemId = $(this).parent().data('id');
-
-        todoList.filter(itemId => itemId !== itemId); 
-        $(this).parent().remove('li'); 
-        //todoList.splice(todoList.value == itemId);
-        // button의 parent를 remove  
-        
-        console.log( $(this).parent().data('id')+"삭제? ");
-        // todoList = todoList.filter(item => item !== value);
-        // 2. filter를 사용하는 방법 ?? 
-
-
-        // 로컬스토리지에 저장하는거까지 
-        saveTodoList(itemId);
+        db.removeItem(itemId);
        
     });
     
     $(this).on('click','.todo-list-add-btn',function(e){
-        addTodo($('input[id=todoInput]').val());
-        $('input[id=todoInput]').val('');
+        
+        addTodo($('#todoInput').val());
     });
 
-
-    loadTodoList();    
+    // loadTodoList();    
     listUp();
 });
