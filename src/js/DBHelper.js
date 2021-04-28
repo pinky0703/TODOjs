d96b3b22a1685c781fe6c051bfde2670fba5fe93
pinky0703 <pinky0703@naver.com>
2021-04-29 05:38:23 +0900

    21-04-28 수업후
    업데이트 기능추가
    CSS 수정 - bootstrap, item...

--- src/js/DBHelper.js
+++ src/js/DBHelper.js
@@ -1,25 +1,31 @@
 class DBHelper{
+    //TODO : 생성자 (파라미터)
+    // 변수이름 앞에 #붙이면 private변수 선언
     #dataList = [];
     #KEY = '';
     constructor(KEY, uiCallback){
         this.#KEY = KEY;
-        let strTodo = localStorage.getItem(this.#KEY); 
-        this.#dataList = (strTodo === null) ? []:  JSON.parse(strTodo);
+        let strTodo = localStorage.getItem(KEY);
+        this.#dataList = (strTodo===null)? [] : JSON.parse(strTodo);
         this.uiCallback = uiCallback;
     }
-    
+    //JAVASCRIPT, PYTHON 은 class 내부에 변수선언을 하지 않아도 접근 가능
+    // testClass = new DBHelper("HI");
+    // testClass.KEY : public변수 사용 //HI
+
     /**
-     * #dataList 리턴 
+     * #dataList 리턴
      */
     get(){
         return this.#dataList;
     }
 
     /**
-     * localStorage 에 KEY 값으로 #dataList 를 문자열로 저장
+     * localStorage에서 KEY값으로 #dataList를 문자열로 저장 
      */
     save(){
         localStorage.setItem(this.#KEY, JSON.stringify(this.#dataList));
+        this.uiCallback();
     }
 
     /**
@@ -37,19 +43,24 @@ class DBHelper{
      * @param {boolean} isChecked 
      */
     updateIsDone(itemId, isChecked){
-        this.#dataList.filter(f=>f.id==itemId).forEach(it => it.isDone = isChecked);
+        this.#dataList.filter(f=>f.id==itemId).forEach(it=>{it.isDone = isChecked});
         this.save();
-        // 회색으로 바꿔주는거 
-        // this.#dataList.filter(f=>f.id==itemId).forEach(it => it.isDone = isChecked);
     }
-
+    /**
+     * itemId 에 해당하는 item 의 완료체크 업데이트 
+     */
+    updateText(itemId, editText){
+        this.#dataList.filter(f=>f.id==itemId).forEach(it=>it.text = editText);
+        this.save();
+    }
+    
     /**
      * itemId 에 해당하는 item 삭제 
      * @param {string} itemId 
      */
     removeItem(itemId){
-        this.#dataList = this.#dataList.filter(f=>f.id !== itemId); 
+        this.#dataList = this.#dataList.filter(f=>f.id!=itemId);
         this.save();
     }
-}
 
+}
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,112 +1,97 @@
-var db = new DBHelper("todoList");
-/**
- * loadTodoList
- * 페이지 로드 다된다음에 localStorage의 데이터를 리스트로 뿌려줌 
- * @param {String} todoText 
- */
-  function  loadTodoList(){ 
-    db.get();
-  }
 
+var db = new DBHelper("todoList",uiUpdate);
 
-  
-  function addListItem(item, uiUpdate){
-
-    let checkedStr = item.isDone ? ' checked ' : '';
-
-    $('#todoList').append(`<li class="list-group-item" data-id="${item.id}">
-    <div class="input-group">
-        <div>
-            <input class="form-check form-switch" type="checkbox" value="" ${checkedStr}>
+function addListItem(item){
+    let checkedStr = item.isDone ? ' checked' : '';
+    let color      = checkedStr  ? 'lightgray' : 'white';
+    // 위에 추가 : prepend
+    // 아래로 추가 : append
+    $('#todoList').append(`
+    <li class='list-group-item todoLi' data-id="${item.id}" style="background-color: ${color}" >
+        <div class="input-group">
+            <div>
+                <input class="form-check form-switch todo-flex" type="checkbox" ${checkedStr}>
+            </div>
+            <span class="todoText"> ${item.text} </span>
+            
+            <input type="text" class="todoTextInput form-control form-control-sm d-none" />
+            <button type="button" class="btn btn-outline-danger btn-sm todo-del-btn"><i class="fas fa-minus-circle"></i></button>
         </div>
-        <span class="todoText">${item.text}</span>
-        <input type="text" class="form-control form-control-sm" />
-        <button type="button" class="btn btn-outline-danger btn-sm todo-del-btn"><i class="fas fa-minus-circle"></i></button>
-    </div>
-    </li>`); 
-    
-
+    </li>`);
 }
-function addEvent(){
 
-    addTodo($('#todoInput').val());
-    $('#todoInput').val('');
-
-}
-function listUp(){
-
-  db.get().forEach(item=>{
-      console.log(item.text);
-      addListItem(item);
-  });
-
+function uiUpdate(){
+    $('#todoList').html(''); //ui에서 List clear
+    db.get().forEach(item=>addListItem(item)); //db값으로 redraw
 }
 
 /**
- * 할일 추가
- * @param {String} todoText 
+ * 할 일 추가
+ * @param {string} todoText 
  */
 function addTodo(){
-    // 위에부터 추가 prepend
-    // 아래에 추가 append
-    // json item 
-    todoText = $('#todoInput').val();
-    if( todoText != ''){
-        todoText.val(''); 
-        let item = {
-            'id': getTodoId(),
-            'text' : todoText,
-            'isDone' : false //  완료 됐는지 안됐는지 ? 
-        }
-        db.addItem(item);
+    let todoText = $("#todoInput").val();
+    $("#todoInput").val('');
+    let item = {
+        'id'    : getTodoId(),
+        'text'  : todoText,
+        'isDone': false
     }
+    //list에 display
+    addListItem(item);
+    db.addItem(item);
+}
 
-    // 이거추가 
-  
+function getCurrentItemId(jElement){
+    return jElement.closest('li.todoLi').data('id');
 }
-var isDigit = function(str){
-    try{
-        parseInt(str);
-        return true;
-    }catch(e){
-        return false;
-    }
-   
-}
-var uiUpdate = function () { // 가상 벌츄어 돔 
-    $('#todoList').html('');
-    db.get().forEach(item=>addListItem(item));
-}
+
 $(document).ready(function(){
-    $('#todoInput').keydown(function(e){
-        if(e.keyCode === 13){
-            addTodo();
-        }
-    });
-    // $('#todoInput').keydown(e => if(e === 13){ addTodo()}});
-    $('.todo-list-add-btn').click(addTodo);
 
-    $(this).on('change','.todoCheck',function(e){
-        // data-id 갖고오는 법 ? 
-        let itemId = $(this).parent().data('id'); // 속성이 data-id 의 값을 가져옴 //  콘솔로 문법 확인가능함 
-        let isChecked = $(this).prop('checked');
+    $('#todoInput').keydown(e=>{if(e.keyCode==13) addTodo();});
+    $('.add').click(addTodo);
+
+    //CHECK
+    $(this).on("change",".todoCheck",function(e){
+        let itemId = getCurrentItemId($(this).closest('li.todoLi').data('id'));
+        // let itemId = $(this).closest('li.todoLi').data('id'); //속성이 data-id의 값을 가져옴 // 붙여서 써야함 
+        let isChecked = $(this).prop("checked");  // $(this).is(":checked")도 가능
+        //배경 흰색으로
+        if(isChecked) 
+            $(this).parent().css("background-color","gray");
+        else
+            $(this).parent().css("background-color","white");
+        // js변수에 해당 item isDone을 업데이트
         db.updateIsDone(itemId, isChecked);
 
-        // 색도 새로 고침해도 저장되게! 
     });
 
-    $(this).on('click','.todo-del-btn',function(e){
-        let itemId = $(this).parent().data('id');
+    //DELETE
+    $(this).on("click",".todo-del-btn",function(e){
+        let itemId = $(this).parent().data('id'); //속성이 data-id의 값을 가져옴
         db.removeItem(itemId);
-        $(this).parent().remove(); 
-       
-    });
-    
-    $(this).on('click','.todo-list-add-btn',function(e){
-        addTodo();
+        //display delete
+        $(this).parent().remove();
     });
 
-    // loadTodoList();    
-    // listUp();
+    $(this).on("click",".todoText",function(e){
+       // let input = $(this).parent().find('.todoTextInput'); parent 사이에 뭔가 생기면 못찾음 
+       let input = $(this).closest('li.todoLi').find('.todoTextInput');
+       input.val($(this).text());
+       input.removeClass('d-none'); // show, hide 해도되고 
+       $(this).addClass('d-none');
+       input.focus();
+    });
+
+    //바깥으로 빠져나갔을때 저장되는 이벤트
+    $(this).on('blur','.todoTextInput',function (e) {
+        let editText = $(this).val(); 
+        // id 가꼬와
+        let itemId = getCurrentItemId($(this));
+        console.log(itemId);
+        db.updateText(itemId,editText);
+    });
+
+    // 저장돼있는 todoList를 UI 표시함 
     uiUpdate();
 });
